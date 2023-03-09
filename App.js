6ed963e0cf082eb52f2ff04812b5d0bb74cd96e4
joscript <josua.lagat@gmail.com>
2023-03-09 15:48:51 +0800

    feature: initial layout, installed eslint prettier for react-native

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  Text,
+  View,
+  StatusBar,
+} from "react-native";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searh}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    flexDirection: "column",
+    paddingTop: isAndroid ? StatusBar.currentHeight : 0,
+  },
+  searh: {
+    backgroundColor: "green",
+    padding: 16,
+  },
+  list: {
+    backgroundColor: "blue",
+    flex: 1,
+    padding: 16,
   },
 });
