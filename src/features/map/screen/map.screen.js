6901db471cb55eb78a2b034019e2f02eb54cdfe6
joscript <josua.lagat@gmail.com>
2023-03-16 13:35:59 +0800

    feature: custom map callout

--- src/features/map/screen/map.screen.js
+++ src/features/map/screen/map.screen.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker, Callout } from "react-native-maps";
 import styled from "styled-components/native";
 
 import { Search } from "../components/search.component";
+import { MapCallout } from "../components/map-callout.component";
 
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
@@ -39,7 +40,20 @@ export const MapScreen = () => {
         }}
       >
         {restaurants.map((restaurant) => {
-          return null;
+          return (
+            <Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            >
+              <Callout>
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
+          );
         })}
       </Map>
     </>
